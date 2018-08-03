7fb2a358a1e13ae7e52580c46a901cb1eaf4695e
pratik <lazy.monk.13@gmail.com>
2018-08-03 10:40:05 -0700

    test 2

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Everyone!'
+    return 'Hello Insight!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
