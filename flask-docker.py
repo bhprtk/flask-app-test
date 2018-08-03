3331d1ecb6e23ba999a782ecb6a2bd3f004102c8
pratik <lazy.monk.13@gmail.com>
2018-08-03 03:58:19 -0700

    test 16

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized containerized 15'
+    return 'Flask Dockerized containerized 16'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
